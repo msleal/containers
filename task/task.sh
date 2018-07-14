296cf1d7fdb14d272131a5c202686757e291f5ee
Marcelo Leal <msl@eall.com.br>
2018-07-14 14:55:50 +0000

    Minor adjustments...

--- task/task.sh
+++ task/task.sh
@@ -3,13 +3,18 @@
 #July 2018
 #
 # EXAMPLE TASK
-# ---------------------------------------------------------------------------------------------------------------------------------------------------
-# This Script executes a login in a Windows Machine and runs a task(s) (the task(s) need to be on the Startup Script for the User).
-# This example Use Case is intended for Graphical Applications that need a User Logged In to run properly (e.g.: Cannot be started as a Service).
+# --------------------------------------------------------------------------------
+# TASK.SH Script executes a login in a Windows Machine and runs a task(s) 
+#      (the task(s) need to be on the Startup Script for the User).
 #
-# IMPORTANT: If the application(s) executed need to keep running, the Startup Script (task(s)) need to execute tsdiscon.exe at the end.
-# ---------------------------------------------------------------------------------------------------------------------------------------------------
-
+# This example Use Case is intended for Graphical Applications that need a User 
+#      Logged In to run properly (e.g.: Cannot be started as a Service).
+#
+# IMPORTANT: If the application(s) executed need to keep running, 
+#            the Startup Script (task(s)) need to execute tsdiscon.exe at the end.
+# --------------------------------------------------------------------------------
 Xvfb :19 -screen 0 "${SCREENRESOLUTION}x16" 2>/dev/null &
 export DISPLAY=:19
-xfreerdp /cert-ignore /u:"${USERNAME}" /p:"${USERPASSWD}" /size:"${SCREENRESOLUTION}" /v:"${HOSTADDR}" 2>/dev/null
+/opt/freerdp-nightly/bin/xfreerdp /cert-ignore /u:"${USERNAME}" /p:"${USERPASSWD}" /size:"${SCREENRESOLUTION}" /v:"${HOSTADDR}" 2>/dev/null
+echo "ETRADE System ONLINE!"
+exit 0
